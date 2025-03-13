df75a0a5069c49b6c67b96d9b3ac6253de78acea
RABUNTHOEUN <rabunthoeun7777@gmail.com>
2025-03-13 19:07:18 +0700

    init

---
+++ resources/js/pages/admin/product/ProductPage.tsx
@@ -0,0 +1,15 @@
+import AppLayout from '@/layouts/app-layout'
+
+const ProductPage = () => {
+    return (
+        <AppLayout>
+            Hello Product
+        </AppLayout>
+    )
+}
+
+export default ProductPage
+
+
+
+
